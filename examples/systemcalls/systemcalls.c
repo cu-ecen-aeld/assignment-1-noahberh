f1562f5c85777ffcb556b466da96889c66dd153b
noah <noahberh@gmail.com>
2024-12-27 15:57:35 -0800

    assignment 3

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,12 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+    bool status = false;
+    if(cmd != NULL)
+    {
+		status = system(cmd) == 0;
+    }
+    return status;
 }
 
 /**
@@ -39,29 +43,39 @@ bool do_exec(int count, ...)
     va_list args;
     va_start(args, count);
     char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
+   
+    for(int i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
-
     va_end(args);
-
-    return true;
+    
+    pid_t pid = fork();
+    if(pid < 0)
+    {
+		syslog(LOG_ERR,"fork() failed");
+    }
+    else if(pid == 0)
+    {
+		execv(command[0],command);
+		perror("execv");          
+        exit(EXIT_FAILURE); 
+    }
+    else
+    {
+       int waitStatus;
+   	   if(waitpid(pid, &waitStatus, 0) == -1)
+   	   {
+           syslog(LOG_ERR,"waitpid() failed");
+           return false;          
+	   } 
+	   else if(WIFEXITED(waitStatus))
+	   {
+		   return (WEXITSTATUS(waitStatus) == 0);
+       }   
+   }
+   return false;
 }
 
 /**
@@ -71,29 +85,64 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
-    }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+	if(outputfile != NULL)
+	{
+		int fd = open(outputfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if(fd < 0)
+		{
+	       syslog(LOG_ERR,"failed to open: %s", outputfile);          
+		}
+		else
+		{
+			va_list args;
+			va_start(args, count);
+			
+			char * command[count + 1];
+            for(int i = 0; i < count; ++i)
+			{
+				command[i] = va_arg(args, char*);
+			}
+			command[count] = NULL;
+            va_end(args);
 
+			pid_t pid = fork();
+			if(pid < 0)
+			{
+		        syslog(LOG_ERR,"fork() failed");          
+				close(fd);
+			}
+			else if (pid == 0)
+			{
+			    syslog(LOG_INFO,"Child process PID: %d",getpid());          
+				if(dup2(fd, STDOUT_FILENO) < 0)
+				{
+				    syslog(LOG_ERR,"fork() failed");          		
+				}
+				close(fd);
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+				execv(command[0], command);
+			    perror("execv");
+                exit(EXIT_FAILURE);           
+			}
+			else
+			{
+				close(fd);
+				int waitstatus;
+				if(waitpid(pid, &waitstatus, 0) == -1)
+				{
+		            syslog(LOG_ERR,"waitpid() failed");
+                    return false;
+				}
+				else if(WIFEXITED(waitstatus))
+				{
+		        	return (WTERMSIG(waitstatus) == 0);					
+				}
+			}        
+		    
+		}
+	}
+   
+    return false;
+}  
+         
 
-    va_end(args);
-
-    return true;
-}
--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
 
 bool do_system(const char *command);
 
